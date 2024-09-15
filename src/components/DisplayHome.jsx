b012bd859e4670113a16fb7cf26d75a90768f72c
Samrat Belbase <samratbelbase408@gmail.com>
2024-09-15 20:13:50 +0545

    SongData is also added successfully

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -3,6 +3,7 @@ import Navbar from "./Navbar";
 import { albumsData } from "../assets/assets";
 import AlbumItem from "./AlbumItem";
 import { songsData } from "../assets/assets";
+import SongItems from "./SongItems";
 
 function DisplayHome() {
   return (
@@ -23,7 +24,17 @@ function DisplayHome() {
       </div>
       <div className="mb-4 "></div>
       <h1 className="my-5 font-bold text-2xl">Todays biggest hits</h1>
-      <div className="flex overflow-auto">{}</div>
+      <div className="flex overflow-auto">
+        {songsData.map((item, index) => (
+          <SongItems
+            key={index}
+            image={item.image}
+            name={item.name}
+            desc={item.desc}
+            id={item.id}
+          />
+        ))}
+      </div>
     </>
   );
 }
--- src/components/SongItems.jsx
+++ src/components/SongItems.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function SongItems({ name, image, desc, id }) {
   return (
-    <div className="min-w-[180] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]">
+    <div className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]">
       <img className="rounded" src={image} alt="" />
       <p className="mt-2 font-bold mb-1">{name}</p>
       <p className="text-sm text-slate-200">{desc}</p>
